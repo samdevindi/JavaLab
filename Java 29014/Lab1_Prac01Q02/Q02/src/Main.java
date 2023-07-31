fd7dde2df396d5a12fbd040140281e408af6497a
samdevindi <136637706+samdevindi@users.noreply.github.com>
2023-07-31 15:47:34 +0530

    Add files via upload

---
+++ Java 29014/Lab1_Prac01Q02/Q02/src/Main.java
@@ -0,0 +1,8 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Maleesha Devindi");
+        System.out.println("Management Information Systems");
+    }
+}
